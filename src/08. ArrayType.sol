e5677e0429a9405c011ec353f0b6608994d0fc4d
Aleksei Besshapov <aleksei.besshapov@pinely.com>
2024-12-08 02:11:18 +0300

    eighth optimization

--- src/08. ArrayType.sol
+++ src/08. ArrayType.sol
@@ -14,7 +14,13 @@ contract ArrayType is IArrayType {
 }
 
 contract ArrayTypeOptimized is IArrayType {
-    /* YOUR SOLUTION GOES HERE */
+    uint256[200] array;
 
-    function initArray() public {}
+    function initArray() public {
+        unchecked {
+            for (uint256 i; i < 200; ++i) {
+                array[i] = i;
+            }
+        }
+    }
 }
