c0bdee71ece6ff3ef558e6988edc78be3af95195
Aleksei Besshapov <aleksei.besshapov@pinely.com>
2024-12-08 01:27:43 +0300

    fifth optimization

--- src/05. PackVariables.sol
+++ src/05. PackVariables.sol
@@ -11,9 +11,14 @@ contract PackVariables is IPackVariables {
     bytes14 five;
     uint8 six;
 
-    function setValues(uint8 _one, uint256 _two, bytes18 _three, uint8[30] calldata _four, bytes14 _five, uint8 _six)
-        public
-    {
+    function setValues(
+        uint8 _one,
+        uint256 _two,
+        bytes18 _three,
+        uint8[30] calldata _four,
+        bytes14 _five,
+        uint8 _six
+    ) public {
         one = _one;
         two = _two;
         three = _three;
@@ -24,9 +29,26 @@ contract PackVariables is IPackVariables {
 }
 
 contract PackVariablesOptimized is IPackVariables {
-    /* YOUR SOLUTION GOES HERE */
+    uint8 one;
+    bytes18 three;
+    uint256 two;
+    bytes14 five;
+    uint8 six;
+    uint8[30] four;
 
-    function setValues(uint8 _one, uint256 _two, bytes18 _three, uint8[30] calldata _four, bytes14 _five, uint8 _six)
-        public
-    {}
+    function setValues(
+        uint8 _one,
+        uint256 _two,
+        bytes18 _three,
+        uint8[30] calldata _four,
+        bytes14 _five,
+        uint8 _six
+    ) public {
+        one = _one;
+        two = _two;
+        three = _three;
+        four = _four;
+        five = _five;
+        six = _six;
+    }
 }
